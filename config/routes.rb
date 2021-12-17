ad1d144193fe99811593d4c89d40b966e29f1fc1
gbukov <gleb.liffe@gmail.com>
2021-12-17 13:26:24 +0100

    Added: i18n support + 2 translations for popup sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
-  root "homes#index"
-
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # i18n
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root "homes#index"
+
+    devise_for :users
+  end
+
 end
