904f1388b69293a1763b50b20797bc2d62f5b9e1
domia0 <95084307+domia0@users.noreply.github.com>
2021-12-21 13:03:10 +0100

    changed meme validation, only allows de or en

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -6,6 +6,6 @@ class Meme < ApplicationRecord
   has_many :reports, as: :reportable
   has_and_belongs_to_many :tags
 
-  validates :lang, presence: true, length: { minimum: 2, maximum: 2 }
+  validates :lang, presence: true, length: { minimum: 2, maximum: 2 }, format: { with: /de|en/, message: "only allows en or de"}
   
 end
