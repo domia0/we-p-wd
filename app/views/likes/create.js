7fb465e123f78c6256bd64f95fbe4fed23e1eca9
domia0 <95084307+domia0@users.noreply.github.com>
2021-12-27 23:37:11 +0100

    like_works_for_memes_show

--- app/views/likes/create.js
+++ app/views/likes/create.js
@@ -1,12 +1,1 @@
-window.addEventListener("load", () => {
-    const element = document.querySelector("#memes-body");
-    element.addEventListener("ajax:success", (event) => {
-      const [_data, _status, xhr] = event.detail;
-      element.insertAdjacentHTML("beforeend", xhr.responseText);
-    });
-    element.addEventListener("ajax:error", () => {
-      element.insertAdjacentHTML("beforeend", "<p>ERROR</p>");
-    });
-  });
-  
-  
+$( "#like-count" ).load( window.location.href + " #like-count" );
