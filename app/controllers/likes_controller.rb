5cc82eb199ea09fde37c9ef5a415c7d847182648
gbukov <gleb.liffe@gmail.com>
2021-12-28 11:37:43 +0100

    Added: add/remove like (frontend) for author. users

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,20 @@
 class LikesController < ApplicationController
-    def new
-        @like = Like.new
-    end
 
-    def create
-        @meme = Meme.find(params[:meme_id])
-        @like = current_user.likes.create!(likeable: @meme)
-    end
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @meme = Meme.find(params[:meme_id])
+    @like = current_user.likes.create!(likeable: @meme)
+  end
+
+  def destroy
+    @meme = Meme.find(params[:meme_id])
+    @like = Like.where(user_id: current_user.id, likeable_id: @meme.id)
+    @like[0].destroy if @like.count == 1
+    # render json: @like
+    # redirect_to root_path
+  end
+
 end
