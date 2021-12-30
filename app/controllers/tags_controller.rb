ddcd0d3087ea80b2e137aee0ba73af3641668540
domia0 <95084307+domia0@users.noreply.github.com>
2021-12-30 18:04:01 +0100

    tag_create_update

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,14 @@
 class TagsController < ApplicationController
+
+    def create
+        @meme = Meme.find(params[:meme_id])
+        @tag = @meme.tags.create(name: params[:tag][:name])
+    end
+    
+   # private 
+  #  def tag_params
+    #    params.require(:tag).permit(meme_ids[])
+    #end
+
+    
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,6 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :memes
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :user_id, uniqueness: true
 
 end
