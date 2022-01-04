34ed4d095a56cf11aea48746a361af680046e98e
domia0 <95084307+domia0@users.noreply.github.com>
2022-01-04 16:07:28 +0100

    fix like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,5 +3,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
   
-  validates :user_id, uniqueness: { scope: :likeable_id }
+  validates :user_id, uniqueness: { scope: :likeable }
 end
